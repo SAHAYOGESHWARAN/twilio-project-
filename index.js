62daeb60651ee7e9b7eb6aee25db14d281e20df0
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-15 15:08:19 +0530

    twlio

---
+++ index.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const express = require('express');
+const session = require('express-session')
+const flash = require('connect-flash')
+
+const port = process.env.PORT || 3000
+const app = express();
+
+app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(session({ 
+    secret: "please log me in",
+    resave: true,
+    saveUninitialized: true
+    }
+));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user
+    next();
+});
+
+//define error handler
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        error : err
+    })
+})
+
+//listen on port
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`)
+});
